9456f6a72af600349a49055753a0d1e6744621b2
AwkwardMongoose <82236528+AwkwardMongoose@users.noreply.github.com>
2024-09-16 20:22:37 -0400

    Add files via upload

--- char/fayruz.js
+++ char/fayruz.js
@@ -4,14 +4,32 @@ var fayruz = {
             "name": "Air Blast",
             "id": "air",
             "type": "simple",
-            "burn": 2
+            "burn": 0
         },
         {
             "name": "Electric Blast",
             "id": "electric",
             "type": "simple",
             "burn": 0
-        }
+        },
+        {
+            "name": "Hurricane Blast",
+            "id": "hurricane",
+            "type": "composite",
+            "burn": 2
+        },
+        {
+            "name": "Lightning Blast",
+            "id": "lightning",
+            "type": "composite",
+            "burn": 2
+        },
+        {
+            "name": "Thunderstorm Blast",
+            "id": "thunderstorm",
+            "type": "composite",
+            "burn": 2
+        },
     ],
     "inf": [
         {
@@ -21,11 +39,29 @@ var fayruz = {
             "burn": 1
         },
         {
+            "name": "Gusting Infusion",
+            "type": "Substance",
+            "id": "gusting",
+            "burn": 1
+        },
+        {
             "name": "Torrent",
             "type": "Form",
             "id": "torrent",
             "burn": 2
-        }
+        },
+        {
+            "name": "Magnetic Infusion",
+            "type": "Substance",
+            "id": "magnetic",
+            "burn": 2
+        },
+        {
+            "name": "Extreme Range",
+            "type": "Form",
+            "id": "extremerange",
+            "burn": 2
+        },
     ],
     "meta": [
         {
